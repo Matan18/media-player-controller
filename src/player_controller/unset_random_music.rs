bf28e011c72c224ae880a8fab8f5785266b46a4d
Mateus Andriola <mateusandriola18@gmail.com>
2023-07-05 21:12:48 -0300

    feat(player_controller/unset_random_music): handle unset_random_music event

---
+++ src/player_controller/unset_random_music.rs
@@ -0,0 +1,30 @@
+use std::env;
+
+use reqwest::{header::HeaderMap, Client};
+
+use crate::util::get_refreshed_token::get_refreshed_token;
+
+const SPOTIFY_NEXT_TRACK: &'static str = "/me/player/shuffle";
+
+pub async fn unset_random_music() -> Result<reqwest::Response, reqwest::Error> {
+    let spotify_player_base_url = env::var("SPOTIFY_PLAYER_BASE_URL").unwrap();
+
+    let access_token = get_refreshed_token().await;
+
+    let authoriation_header = format!("Bearer {}", access_token);
+    let mut headers = HeaderMap::new();
+    headers.insert("authorization", authoriation_header.parse().unwrap());
+    headers.insert("Content-length", "0".parse().unwrap());
+
+    let query = &[("state", "false")];
+
+    let url = format!(
+        "{}{}",
+        spotify_player_base_url,
+        SPOTIFY_NEXT_TRACK.to_string()
+    );
+    let client = Client::new();
+    let response = client.put(url).query(query).headers(headers).send().await;
+
+    return response;
+}
