0c8cfb6679524bc3be5b83d377945f4ff472376e
Mateus Andriola <mateusandriola18@gmail.com>
2023-07-07 18:37:21 -0300

    feat(main): startup controll_handler

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 extern crate dotenv;
 
 use dotenv::dotenv;
+use std::thread;
 
+use controll_handler::controll_handler;
 use token::{oauth_start::oauth_start, success_callback::success_callback};
 
 #[macro_use]
 extern crate rocket;
 
+mod controll_handler;
+mod player_controller;
 mod token;
 mod util;
 
@@ -14,10 +18,16 @@ mod util;
 pub async fn main() -> Result<(), rocket::Error> {
     dotenv().ok();
 
+    let handle = thread::spawn(|| {
+        controll_handler();
+    });
+
     let _rocket = rocket::build()
         .mount("/", routes![oauth_start, success_callback])
         .launch()
         .await?;
+
+    handle.join().unwrap();
 
     Ok(())
 }
